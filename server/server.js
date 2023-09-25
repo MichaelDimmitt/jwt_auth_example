0e1c11a847478fc9e21e7d99f53815dd0501c37c
michael <michaelgdimmitt@gmail.com>
2023-09-25 12:43:41 -0400

    fix routes

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes')(app);
+require('./routes/index.js')(app);
 
 app.listen(port, () => console.log(`LISTENING ON PORT ${port}`));
 
